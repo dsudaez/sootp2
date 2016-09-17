74eb036535fb19af44bc79f6580432238148da77
Ezequiel Montes <emontes@theinit.com>
2016-09-16 23:55:41 -0300

    Create maven project using simple archetype.

---
+++ src/main/java/ar/edu/unju/fi/soo/App.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.soo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
