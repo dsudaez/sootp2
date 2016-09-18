2cf26fb1b6fea1577a1f6509bf3124c5843e7d93
Jorge Riera <jorge.riera9@gmail.com>
2016-09-18 19:51:23 -0300

    modificando visibilidad atributo Plan y Fee, agregando metodos getNextDateDue e implementando GenereteFee del plan regular

--- src/main/java/ar/edu/unju/fi/soo/model/Plan.java
+++ src/main/java/ar/edu/unju/fi/soo/model/Plan.java
@@ -1,12 +1,15 @@
 package ar.edu.unju.fi.soo.model;
 
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public abstract class Plan {
-    private Vehicle vehicle;
-    private Client client;
-    private List<Fee> fees = new ArrayList<Fee>();
+    protected Vehicle vehicle;
+    protected Client client;
+    protected List<Fee> fees = new ArrayList<Fee>();
     
     public List<Fee> getUnpaidFees() {
         List<Fee> unpaidFees = new ArrayList<Fee>();
@@ -19,8 +22,15 @@ public abstract class Plan {
 
         return unpaidFees;
     }
-
-    public abstract void generateFees();
+    
+    public Date getNextDueDate(Date fecha){
+    	Calendar calendar = Calendar.getInstance();
+    	calendar.setTime(fecha); // Configuramos la fecha que se recibe
+    	calendar.add(Calendar.DAY_OF_YEAR, 30);  // numero de días a añadir, o restar en caso de días<0
+    	return (Date) calendar.getTime(); // Devuelve el objeto Date con los nuevos días añadidos
+    
+    }
+    public abstract void generateFees(int feesAmount);
 
     public abstract Double calculateChargeToPay();
 
@@ -47,5 +57,7 @@ public abstract class Plan {
     public void setFees(List<Fee> fees) {
         this.fees = fees;
     }
+
+	
 
 }
--- src/main/java/ar/edu/unju/fi/soo/model/PlanRegular.java
+++ src/main/java/ar/edu/unju/fi/soo/model/PlanRegular.java
@@ -1,17 +1,33 @@
 package ar.edu.unju.fi.soo.model;
 
+import java.util.Date;
+
 public class PlanRegular extends Plan {
 
     @Override
-    public void generateFees() {
+    public void generateFees(int feesAmount) {
         // TODO Auto-generated method stub
-        
+        if ((feesAmount >= 60) && (feesAmount <= 80)){
+        	Double precio = this.vehicle.getValue();
+        	
+        	Date hoy = new Date();
+        	Fee feetoadd1 = new Fee(0,precio,hoy);
+        	this.fees.add(feetoadd1);
+        	
+        	for (int i = 1; i < feesAmount; i++) {
+        		Date fecha = getNextDueDate(this.fees.get(i-1).getDueDate()) ;
+        		Fee feetoadd = new Fee(i,precio,fecha);
+        		this.fees.add(feetoadd); 
+			}
+        }  
     }
-
+    
     @Override
     public Double calculateChargeToPay() {
         // TODO Auto-generated method stub
         return null;
     }
+    
+     	
 
 }
