1d6f9acb287eb2ba4cc5bcd92189433a3eac59b4
Jorge Riera <jorge.riera9@gmail.com>
2016-09-18 20:18:12 -0300

    implementando calcuateChargeToPay en PlanRegular y para Plan7030

--- src/main/java/ar/edu/unju/fi/soo/model/Plan7030.java
+++ src/main/java/ar/edu/unju/fi/soo/model/Plan7030.java
@@ -1,17 +1,38 @@
 package ar.edu.unju.fi.soo.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Plan7030 extends Plan {
 
-    @Override
-    public void generateFees() {
-        // TODO Auto-generated method stub
-        
+	@Override
+	public void generateFees(int feesAmount) {
+		if ((feesAmount >= 60) && (feesAmount <= 80)){
+        	Double precio = this.vehicle.getValue() * 0.7;
+        	Date hoy = new Date();
+        	Fee feetoadd1 = new Fee(0,precio,hoy);
+        	this.fees.add(feetoadd1);
+        	
+        	for (int i = 1; i < feesAmount; i++) {
+        		Date fecha = getNextDueDate(this.fees.get(i-1).getDueDate()) ;
+        		Fee feetoadd = new Fee(i,precio,fecha);
+        		this.fees.add(feetoadd); 
+			}
+        }  
+		
+	}
+	@Override
+    public Double calculateChargeToPay() {
+		List<Fee> unpaidFees = new ArrayList<Fee>();
+    	unpaidFees = this.getUnpaidFees();
+    	Double money=0.;
+    	for (Fee fee : unpaidFees) {
+    		money= money + fee.getAmount();
+    	}
+    	return money + (this.vehicle.getValue()*0.3);
     }
 
-    @Override
-    public Double calculateChargeToPay() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	
 
 }
--- src/main/java/ar/edu/unju/fi/soo/model/PlanRegular.java
+++ src/main/java/ar/edu/unju/fi/soo/model/PlanRegular.java
@@ -1,15 +1,15 @@
 package ar.edu.unju.fi.soo.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class PlanRegular extends Plan {
 
     @Override
     public void generateFees(int feesAmount) {
-        // TODO Auto-generated method stub
         if ((feesAmount >= 60) && (feesAmount <= 80)){
         	Double precio = this.vehicle.getValue();
-        	
         	Date hoy = new Date();
         	Fee feetoadd1 = new Fee(0,precio,hoy);
         	this.fees.add(feetoadd1);
@@ -24,8 +24,13 @@ public class PlanRegular extends Plan {
     
     @Override
     public Double calculateChargeToPay() {
-        // TODO Auto-generated method stub
-        return null;
+    	List<Fee> unpaidFees = new ArrayList<Fee>();
+    	unpaidFees = this.getUnpaidFees();
+    	Double money=0.;
+    	for (Fee fee : unpaidFees) {
+    		money= money + fee.getAmount();
+    	}
+    	return money;
     }
     
      	
