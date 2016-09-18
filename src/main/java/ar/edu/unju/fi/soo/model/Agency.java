7043e31211fe1b392cbe28acbbf2cadd3c12274e
debit0 <deb.sudaez@gmail.com>
2016-09-18 19:52:12 -0300

    Agregando métodos getMoneyReceivable() y getLeastdebtor()

--- src/main/java/ar/edu/unju/fi/soo/model/Agency.java
+++ src/main/java/ar/edu/unju/fi/soo/model/Agency.java
@@ -5,6 +5,32 @@ import java.util.List;
 
 public class Agency {
     private List<Plan> plans = new ArrayList<Plan>();
+    
+    public Double getMoneyReceivable (){
+        Double moneyReceivable = 0.0;
+        for (Plan plan : plans){
+         moneyReceivable += plan.calculateChargeToPay();
+        }
+        return moneyReceivable;
+       }
+       
+    public Client getLeastDebtor(){
+        Plan selectedPlan = plans.get(0);
+        for (Plan plan : plans){
+         if (plan.calculateChargeToPay() < selectedPlan.calculateChargeToPay()){
+          selectedPlan = plan; 
+         }
+        }
+        return selectedPlan.getClient();
+       }
+    
+    public void createPlan7030 (){
+    	
+    }
+    
+    public void createPlanRegular (){
+    	
+    }
 
     public List<Plan> getPlans() {
         return plans;
