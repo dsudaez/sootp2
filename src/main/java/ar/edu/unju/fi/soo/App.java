451b87f4b2cbeb584425093576284a550fa02748
debit0 <deb.sudaez@gmail.com>
2016-09-18 22:32:57 -0300

    modificando app

--- src/main/java/ar/edu/unju/fi/soo/App.java
+++ src/main/java/ar/edu/unju/fi/soo/App.java
@@ -1,7 +1,10 @@
 package ar.edu.unju.fi.soo;
 
+import java.util.ArrayList;
+
 import ar.edu.unju.fi.soo.model.Agency;
 import ar.edu.unju.fi.soo.model.Client;
+import ar.edu.unju.fi.soo.model.Fee;
 import ar.edu.unju.fi.soo.model.Plan;
 import ar.edu.unju.fi.soo.model.PlanRegular;
 import ar.edu.unju.fi.soo.model.Vehicle;
@@ -11,9 +14,9 @@ import ar.edu.unju.fi.soo.model.Vehicle;
  *
  */
 public class App {
-	Agency agency = new Agency();
+	 static Agency agency = new Agency();
 
-	public void addSamplePlanRegular() {
+	public static  void addSamplePlanRegular() {
 		Vehicle chevroletCamaro = new Vehicle();
 		chevroletCamaro.setValue(1285000.0);
 
@@ -22,45 +25,63 @@ public class App {
 		int feesAmount = 78;
 
 		PlanRegular plan = agency.createPlanRegular(chevroletCamaro, client, feesAmount);
+		
+		// pagamos 10 cuotas
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		plan.payNextFee();
+		
+		ArrayList<Fee> unpaidFees=  (ArrayList<Fee>) plan.getUnpaidFees();
+		System.out.println("la cantidad de cuotas que faltan pagar son " + unpaidFees.size());
 	}
 
-	public void addSamplePlan7030() {
+	public static  void addSamplePlan7030() {
 		Vehicle fordMustang = new Vehicle();
 		fordMustang.setValue(1250000.0);
 
 		Client client = new Client();
 		client.setName("Debi");
-		int feesAmount = 78;
+		int feesAmount = 68;
 
 		agency.createPlanRegular(fordMustang, client, feesAmount);
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		Agency agency = new Agency();
-		Vehicle vehicle = new Vehicle();
-		vehicle.setValue(250000.0);
-		Vehicle vehicle2 = new Vehicle();
-		vehicle2.setValue(285000.0);
-
-		Client client = new Client();
-		client.setName("Cliente 1");
-		int feesAmount = 60;
-
-		agency.createPlan7030(vehicle2, client, feesAmount);
-		agency.createPlanRegular(vehicle, client, feesAmount);
-
-		Client client2 = new Client();
-		client2.setName("Cliente 2");
-		feesAmount = 80;
-		agency.createPlan7030(vehicle2, client2, feesAmount);
-		agency.createPlanRegular(vehicle, client2, feesAmount);
-
-		for (Plan plan : agency.getPlans()) {
-			System.out.println(plan);
-		}
-
+//		Agency agency = new Agency();
+//		Vehicle vehicle = new Vehicle();
+//		vehicle.setValue(250000.0);
+//		Vehicle vehicle2 = new Vehicle();
+//		vehicle2.setValue(285000.0);
+//
+//		Client client = new Client();
+//		client.setName("Cliente 1");
+//		int feesAmount = 60;
+//
+//		agency.createPlan7030(vehicle2, client, feesAmount);
+//		agency.createPlanRegular(vehicle, client, feesAmount);
+//
+//		Client client2 = new Client();
+//		client2.setName("Cliente 2");
+//		feesAmount = 80;
+//		agency.createPlan7030(vehicle2, client2, feesAmount);
+//		agency.createPlanRegular(vehicle, client2, feesAmount);
+//
+//		for (Plan plan : agency.getPlans()) {
+//			System.out.println(plan);
+//		}
+		addSamplePlanRegular();
+		addSamplePlan7030();
 		System.out.println("MoneyReceivable: " + agency.getMoneyReceivable());
 		System.out.println("Cliente que menos debe: " + agency.getLeastDebtor());
+		
+		
 	}
 }
