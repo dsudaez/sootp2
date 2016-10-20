af24721cb7456931f540e4995e0b4012b51f7684
Ezequiel Montes <emontes@theinit.com>
2016-10-19 22:39:53 -0300

    Add DAOs for domain classes Fee, Plan and Vehicle.

---
+++ src/main/java/ar/edu/unju/fi/soo/model/dao/impl/FeeDAOImpl.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.soo.model.dao.impl;
+
+import ar.edu.unju.fi.soo.model.Fee;
+import ar.edu.unju.fi.soo.model.dao.BaseDAO;
+
+public class FeeDAOImpl extends BaseHibernate<Fee, Long> implements BaseDAO<Fee, Long> {
+
+}
---
+++ src/main/java/ar/edu/unju/fi/soo/model/dao/impl/PlanDAOImpl.java
@@ -0,0 +1,8 @@
+package ar.edu.unju.fi.soo.model.dao.impl;
+
+import ar.edu.unju.fi.soo.model.Plan;
+import ar.edu.unju.fi.soo.model.dao.PlanDAO;
+
+public class PlanDAOImpl extends BaseHibernate<Plan, Long> implements PlanDAO {
+
+}
