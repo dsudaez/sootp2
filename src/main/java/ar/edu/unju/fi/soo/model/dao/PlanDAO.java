df22406e7c97a7b389996c9a89c7054c9dceccbd
Ezequiel Montes <emontes@theinit.com>
2016-11-23 00:53:58 -0300

    Add a new DAO's method to find all plans by client name.

--- src/main/java/ar/edu/unju/fi/soo/model/dao/PlanDAO.java
+++ src/main/java/ar/edu/unju/fi/soo/model/dao/PlanDAO.java
@@ -1,7 +1,11 @@
 package ar.edu.unju.fi.soo.model.dao;
 
+import java.util.List;
+
 import ar.edu.unju.fi.soo.model.Plan;
 
 public interface PlanDAO extends BaseDAO<Plan, Long> {
+
+	List<Plan> findAllByClientName(String clientName);
 
 }
--- src/main/java/ar/edu/unju/fi/soo/model/dao/impl/PlanDAOImpl.java
+++ src/main/java/ar/edu/unju/fi/soo/model/dao/impl/PlanDAOImpl.java
@@ -1,5 +1,9 @@
 package ar.edu.unju.fi.soo.model.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import ar.edu.unju.fi.soo.model.Plan;
@@ -7,5 +11,16 @@ import ar.edu.unju.fi.soo.model.dao.PlanDAO;
 
 @Repository
 public class PlanDAOImpl extends BaseHibernate<Plan, Long> implements PlanDAO {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Plan> findAllByClientName(String clientName) {
+		Criteria criteria = createCriteria();
+		criteria.createAlias("client", "client");
+		criteria.add(Restrictions.eq("client.name", clientName));
+//		criteria.setMaxResults(1);
+//		return (Plan) criteria.uniqueResult();
+		return criteria.list();
+	}
 
 }
