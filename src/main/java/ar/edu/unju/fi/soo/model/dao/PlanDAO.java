6042d34669b36c5de06f37e3b1f46a6609152c6a
Jorge Riera <jorge.riera9@gmail.com>
2016-11-23 03:24:56 -0300

    implementando metodo payNextFee(idPlan) DAO

--- src/main/java/ar/edu/unju/fi/soo/model/dao/PlanDAO.java
+++ src/main/java/ar/edu/unju/fi/soo/model/dao/PlanDAO.java
@@ -7,5 +7,6 @@ import ar.edu.unju.fi.soo.model.Plan;
 public interface PlanDAO extends BaseDAO<Plan, Long> {
 
 	List<Plan> findAllByClientName(String clientName);
-
+	
+	void payNextFee(long id);
 }
