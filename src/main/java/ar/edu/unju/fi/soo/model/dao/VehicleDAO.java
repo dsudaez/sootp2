8759f4567b44a02a0b8dac011c0260d2bcf79dab
Jorge Riera <jorge.riera9@gmail.com>
2016-11-23 23:27:57 -0300

    add findByCode in VehicleDAO

--- src/main/java/ar/edu/unju/fi/soo/model/dao/VehicleDAO.java
+++ src/main/java/ar/edu/unju/fi/soo/model/dao/VehicleDAO.java
@@ -3,5 +3,5 @@ package ar.edu.unju.fi.soo.model.dao;
 import ar.edu.unju.fi.soo.model.Vehicle;
 
 public interface VehicleDAO extends BaseDAO<Vehicle, Long> {
-
+	Vehicle findByCode(String string);
 }
--- src/main/java/ar/edu/unju/fi/soo/model/dao/impl/VehicleDAOImpl.java
+++ src/main/java/ar/edu/unju/fi/soo/model/dao/impl/VehicleDAOImpl.java
@@ -1,5 +1,9 @@
 package ar.edu.unju.fi.soo.model.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import ar.edu.unju.fi.soo.model.Vehicle;
@@ -7,5 +11,16 @@ import ar.edu.unju.fi.soo.model.dao.VehicleDAO;
 
 @Repository
 public class VehicleDAOImpl extends BaseHibernate<Vehicle, Long> implements VehicleDAO {
+
+	@Override
+	public Vehicle findByCode(String code) {
+		Criteria criteria = createCriteria();
+		criteria.add(Restrictions.eq("code", code));
+		@SuppressWarnings("unchecked")
+		List<Vehicle> list = criteria.list();
+		return list.isEmpty() ? null : list.get(0);
+	}
+
+	
 
 }
